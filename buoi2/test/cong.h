d1185e53493fc945d42cf359b4ff921d05384ad9
Thế Hưng <hung12122004@gmail.com>
2025-02-27 10:07:36 +0700

    baitapbaitap

---
+++ buoi2/test/cong.h
@@ -0,0 +1,4 @@
+#if !defined(cong_h)
+#define cong_h
+int sum(int a, int b);
+#endif
---
+++ buoi2/test/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "cong.h"
+int main()
+{
+    int a = 5, b = 5;
+
+    printf("Tong cua %d va %d la %d\n", a, b, sum(a, b));
+}
